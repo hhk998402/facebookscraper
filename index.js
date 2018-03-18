bb301ef8acaf0e8c3c8f4e4d8383a0a08dce1c02
Harsha Bommana <harshablast@gmail.com>
2018-03-18 07:53:44 +0530

    uncommented some stuff

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const childProcess = require('child_process');
 const fbScraper = require('./bin/facebook-nologin-scraper');
-//const w2vUtils = require('./bin/w2vUtils');
+const w2vUtils = require('./bin/w2vUtils');
 
 //childProcess.execSync('node bin/facebook-nologin-scraper.js https://www.facebook.com/zuck');
 console.log('Done!');
